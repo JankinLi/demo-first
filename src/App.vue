11c8dda7723c900bc335d84422c04bea7be20bdc
jankin.li <jankin.li@outlook.com>
2022-11-03 21:19:12 +0800

    left part, custom event.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <div id="app">
-    <p>hello</p>
+    <p>hello World!!!!</p>
+    <p>全局 {{ count }}</p>
     <hr>
-    <img alt="Vue logo" src="./assets/logo.png"></img>
+    <left_part init_msg="这是左边的部分" @countChanged="ValChanged"></left_part>
+    <hr>
+    <img alt="Vue logo" src="./assets/logo.png">
     <hello msg="Lichuan Welcome."></hello>
   </div>
   
@@ -10,10 +13,23 @@
 
 <script>
 import Hello from "@/components/HelloWorld.vue"
+import Left from "@/components/Left.vue"
 
 export default{
   components:{
     'hello' : Hello,
+    'left_part': Left,
+  },
+  data(){
+    return {
+      count: 0
+    }
+  },
+  methods:{
+    ValChanged(val){
+      //console.log('val changed.')
+      this.count = val
+    }
   }
 }
 </script>
---
+++ src/components/Left.vue
@@ -0,0 +1,37 @@
+<template>
+    <dir id="left" class="box">
+        <h1>This is Left side.</h1>
+        <h4>{{ message }}</h4>
+        <h4>{{ count }}</h4>
+        <button @click="add">click me</button>
+    </dir>
+</template>
+
+<script>
+export default {
+    props:{
+        init_msg:{
+            default: 'Lichuan'
+        },
+    },
+    data(){
+        return {
+            message: this.init_msg,
+            count:0,
+        }        
+    },
+    methods:{
+        add(){
+            this.count += 1;
+            this.$emit('countChanged', this.count)
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+    #left{
+        background-color: pink;
+        height: 300px;
+    }
+</style>
