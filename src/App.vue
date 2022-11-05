0120985754738559703d37d394a3842af6548652
jankin.li <jankin.li@outlook.com>
2022-11-05 12:21:29 +0800

    ref and $nextTick

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <p>hello World!!!!</p>
     <p>全局 count={{ count }}</p>
     <hr>
+    <input type="text" ref="inpRef" v-if="flag" @blur="lostFocus"><button v-else @click="showInput">显示输入框</button>
     <div class="box">
       <Left init_msg="这是左边的部分" @countChanged="valChanged"></Left>
       <Right></Right>
@@ -21,19 +22,36 @@ import Right from "@/components/Right.vue"
 
 export default{
   components:{
+    
     'hello' : Hello,
     Left,
     Right,
   },
   data(){
     return {
+      flag:true,
       count: 0
     }
+  },
+  created(){
+    console.log('created in APP')
+  },
+  mounted(){
+    this.$refs.inpRef.focus()
   },
   methods:{
     valChanged(val){
       //console.log('val changed.')
       this.count = val
+    },
+    showInput(){
+      this.flag=true
+      this.$nextTick(()=>{
+        this.$refs.inpRef.focus()
+      })
+    },
+    lostFocus(){
+      this.flag = false
     }
   }
 }
